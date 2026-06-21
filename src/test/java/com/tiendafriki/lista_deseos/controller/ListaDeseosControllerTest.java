e1cd024463081d3a61b6abca9ad8594574b39b6d
H34RTF1R3 <isa.mejias@duocuc.cl>
2026-06-21 18:45:37 -0400

    Pruebas Unitarias Hechas Misma Rama

--- src/test/java/com/tiendafriki/lista_deseos/controller/ListaDeseosControllerTest.java
+++ src/test/java/com/tiendafriki/lista_deseos/controller/ListaDeseosControllerTest.java
@@ -1,5 +1,44 @@
 package com.tiendafriki.lista_deseos.controller;
 
-public class ListaDeseosControllerTest {
+import com.tiendafriki.lista_deseos.model.ListaDeseos;
+import com.tiendafriki.lista_deseos.service.ListaDeseosServ;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ListaDeseosController.class)
+class ListaDeseosControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ListaDeseosServ service;
+
+    @Test
+    void listarListasDeseos() throws Exception {
+
+        ListaDeseos listaDeseos = new ListaDeseos(
+                1,
+                "11111111-1",
+                LocalDateTime.now(),
+                null
+        );
+
+        List<ListaDeseos> respuesta = List.of(listaDeseos);
+
+        when(service.listar()).thenReturn(respuesta);
+
+        mockMvc.perform(get("/wishlist/listar"))
+                .andExpect(status().isOk());
+    }
 }
---
+++ src/test/java/com/tiendafriki/lista_deseos/controller/WebMvcTest.java
@@ -0,0 +1,7 @@
+package com.tiendafriki.lista_deseos.controller;
+
+public @interface WebMvcTest {
+
+    Class<ListaDeseosController> value();
+
+}
